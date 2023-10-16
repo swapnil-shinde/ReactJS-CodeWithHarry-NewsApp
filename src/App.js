3e4fba2b9ec31286ebb2bdb814cb81e20a4e0097
swapnil-shinde <swapnilshinde7900@gmail.com>
2023-10-16 18:00:19 +0530

    Video 31: Fetching news category wise using router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import React, { Component } from 'react'
 import News from './components/News'
 import NavBar from './components/NavBar'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
 
 export default class App extends Component {
 
@@ -12,8 +17,22 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar hanleCountry={this.hanleCountry}/>
-        <News pageSize={6} category={'science'} country='in'/>
+        <Router>
+          <NavBar hanleCountry={this.hanleCountry}/>
+          {/* <News pageSize={6} category={'general'} country='in'/> */}
+
+          <Routes>
+            <Route exact path='/' element={<News key='general' pageSize={6} category={'general'} country='in'/>} />
+            <Route exact path='/business' element={<News key='business' pageSize={6} category={'business'} country='in'/>} />
+            <Route exact path='/entertainment' element={<News key='entertainment' pageSize={6} category={'entertainment'} country='in'/> } />
+            <Route exact path='/general' element={<News key='general' pageSize={6} category={'general'} country='in'/> } />
+            <Route exact path='/health' element={<News key='health' pageSize={6} category={'health'} country='in'/> } />
+            <Route exact path='/science' element={<News key='science' pageSize={6} category={'science'} country='in'/> } />
+            <Route exact path='/sports' element={<News key='sports' pageSize={6} category={'sports'} country='in'/> } />
+            <Route exact path='/technology' element={<News key='technology' pageSize={6} category={'technology'} country='in'/> } />
+
+          </Routes>
+        </Router>
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export class NavBar extends Component {
 
@@ -16,38 +18,35 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
           <div className="container-fluid d-flex justify-content-between">
-            <a className="navbar-brand" href="/">Navbar</a>
+            <Link className="navbar-brand" to="/">NewsApp</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{width:'75%'}}>
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">About</a>
+                  <Link className="nav-link active" aria-current="page" to="/business">Business</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">business</a>
+                  <Link className="nav-link active" aria-current="page" to="/entertainment">Entertainment</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">entertainment</a>
+                  <Link className="nav-link active" aria-current="page" to="/general">General</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">general</a>
+                  <Link className="nav-link active" aria-current="page" to="/health">Health</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">health</a>
+                  <Link className="nav-link active" aria-current="page" to="/science">Science</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">science</a>
+                  <Link className="nav-link active" aria-current="page" to="/sports">Sports</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">sports</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">technology</a>
+                  <Link className="nav-link active" aria-current="page" to="/technology">Technology</Link>
                 </li>
               </ul>
             </div>
